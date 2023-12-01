a915bd5d917bfa501d501d50d1b5554688aa1fef
beggarsect <144620197+beggarsect@users.noreply.github.com>
2023-12-01 12:57:12 +0100

    changed the priority for condition checks

--- ft_calloc.c
+++ ft_calloc.c
@@ -16,9 +16,9 @@ void	*ft_calloc(size_t num_elements, size_t element_size)
 {
 	void	*ptr;
 
-	ptr = malloc(num_elements * element_size);
 	if (num_elements == 0 || element_size == 0)
-		return (malloc(0));
+		return (NULL);
+	ptr = malloc(num_elements * element_size);
 	if (ptr == NULL)
 		return (NULL);
 	ft_memset((unsigned char *)ptr, 0, num_elements * element_size);
